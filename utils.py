ff932493345287abbd713fa63b6474c55752df88
btreelabs2022 <105281516+btreelabs2022@users.noreply.github.com>
2026-02-01 08:24:50 -0800

    Fix calculate_average function return value
    .

--- utils.py
+++ utils.py
@@ -2,7 +2,8 @@ def calculate_average(numbers):
     total = 0
     for num in numbers:
         total += num
-    return total / len(numbers)
+    return total
 
 def get_user_name(user):
     return user["name"].upper()
+
