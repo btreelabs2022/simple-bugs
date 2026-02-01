6a2b3477474dc575d912039a092f127fd0c14b35
btreelabs2022 <105281516+btreelabs2022@users.noreply.github.com>
2026-02-01 08:53:23 -0800

    change

--- utils.py
+++ utils.py
@@ -4,8 +4,9 @@ def calculate_average(numbers):
     total = 0
     for num in numbers:
         total += num
-    return total / len(numbers)
+    return total
 
 def get_user_name(user):
     return user["name"].upper()
+
 
